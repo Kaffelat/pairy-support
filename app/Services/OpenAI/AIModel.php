85200ac6ed11ea77a7f90b5d2648ec417d8630e0
ghf <simo274e@stud.kea.dk>
2023-04-12 13:07:48 +0200

    Start på CRUD af models og model data service klasserne

--- app/Services/OpenAI/AIModel.php
+++ app/Services/OpenAI/AIModel.php
@@ -1,5 +1,108 @@
 <?php
 namespace App\Services\OpenAI;
+
 use OpenAI;
 
+class AIModel 
+{
+    /**
+    * Makes a new Model in OpenAI
+    */
+    public function makeNewModel($client ,$traningFileId, $validationFileId, $modelType): array
+    {
+        $myAPIKey = getenv('OPENAI_API_KEY');
+        $client = OpenAI::client($myAPIKey);
 
+        if ($validationFileId === TRUE) { 
+            $response = $client->fineTunes()->create([
+                'training_file' => $traningFileId,
+                'validation_file' => $validationFileId,
+                'model' => $modelType,
+                'n_epochs' => 4,
+                'learning_rate_multiplier' => 0.2,
+                'prompt_loss_weight' => 0.01,
+                
+            ]);
+        }
+
+        $response = $client->fineTunes()->create([
+            'training_file' => $traningFileId,
+            'model' => 'curie',
+            'n_epochs' => 4,
+            'learning_rate_multiplier' => 0.2,
+            'prompt_loss_weight' => 0.01,
+        ]);
+
+        $response->id;
+        $response->object; 
+
+        return $response->toArray();
+    }
+
+    /**
+    * Deletes a model 
+    */
+    public function deleteModel($client, $modelId): array
+    {
+        $myAPIKey = getenv('OPENAI_API_KEY');
+        $client = OpenAI::client($myAPIKey);
+
+        $response = $client->models()->delete($modelId);
+
+        $response->id; 
+        $response->object; 
+        $response->deleted; 
+
+        return $response->toArray();
+    }
+
+    /**
+    * Lists all models on OpenAI
+    */
+    public function listModels($client): array
+    {
+        $response = $client->models()->list();
+
+        $response->object; 
+
+        foreach ($response->data as $result) {
+            $result->id; 
+            $result->object; 
+            
+        }
+
+        return $response->toArray(); 
+    }
+
+    /**
+    * Gets a specific model
+    */
+    public function getASpecificModel($client): array
+    {
+        $response = $client->models()->retrieve('text-davinci-003');
+
+        $response->id; // 'text-davinci-003'
+        $response->object; // 'model'
+        $response->created; // 1642018370
+        $response->ownedBy; // 'openai'
+        $response->root; // 'text-davinci-003'
+        $response->parent; // null
+
+        foreach ($response->permission as $result) {
+            $result->id; // 'modelperm-7E53j9OtnMZggjqlwMxW4QG7' 
+            $result->object; // 'model_permission' 
+            $result->created; // 1664307523 
+            $result->allowCreateEngine; // false 
+            $result->allowSampling; // true 
+            $result->allowLogprobs; // true 
+            $result->allowSearchIndices; // false 
+            $result->allowView; // true 
+            $result->allowFineTuning; // false 
+            $result->organization; // '*' 
+            $result->group; // null 
+            $result->isBlocking; // false 
+        }
+
+        return $response->toArray(); 
+    }
+}
--- app/Services/OpenAI/AIModelData.php
+++ app/Services/OpenAI/AIModelData.php
@@ -3,5 +3,59 @@ namespace App\Services\OpenAI;
 
 class AIModelData
 {
-    
+    /**
+    * Makes a new Model in OpenAI
+    */
+    public function uploadAFile($client, $filePath): array
+    {
+        $response = $client->files()->upload([
+            'purpose' => 'fine-tune',
+            'file' => fopen($filePath, 'r'),
+        ]);
+        
+        $response->id; // 'file-XjGxS3KTG0uNmNOK362iJua3'
+        $response->object; // 'file'
+        $response->bytes; // 140
+        $response->createdAt; // 1613779657
+        $response->filename; // 'mydata.jsonl'
+        $response->purpose; // 'fine-tune'
+        $response->status; // 'succeeded'
+        $response->status_details; // null
+
+        fclose($response);
+
+        return $response->toArray(); 
+    }
+
+    /**
+    * Makes a new Model in OpenAI
+    */
+    public function deleteAFile($client, $fileId): array
+    {
+        $response = $client->files()->delete($fileId);
+
+        $response->id; // 'file-XjGxS3KTG0uNmNOK362iJua3'
+        $response->object; // 'file'
+        $response->deleted; // true
+
+        return $response->toArray(); 
+    }
+
+    /**
+    * Makes a new Model in OpenAI
+    */
+    public function listAllFiles($client): array
+    {
+        $response = $client->files()->list();
+
+        $response->object; // 'list'
+
+        foreach ($response->data as $result) {
+            $result->id; // 'file-XjGxS3KTG0uNmNOK362iJua3'
+            $result->object; // 'file'
+            // ...
+        }
+
+        return $response->toArray(); 
+    }
 }
