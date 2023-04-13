0effa4272b969179985e1c49f1f8cea45bb7c160
ghf <simo274e@stud.kea.dk>
2023-04-13 13:38:54 +0200

    Ændret layoutet

--- resources/js/Pages/Models/UploadTraningData.vue
+++ resources/js/Pages/Models/UploadTraningData.vue
@@ -8,7 +8,7 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">Upload traningdata</h2>
+            <h2 id="headline" class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Upload traningdata</h2>
         </template>
 
         <div class="py-12">
