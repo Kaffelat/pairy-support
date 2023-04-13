77ab1adc9ae503ce1bfd62684a9b256a777b0a48
ghf <simo274e@stud.kea.dk>
2023-04-13 13:38:23 +0200

    ændret lidt på ui'en

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -14,18 +14,18 @@ const showingNavigationDropdown = ref(false);
     <div>
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+                <!-- Logo -->
+                <div class="shrink-0 flex justify-center">
+                    <Link :href="route('dashboard')">
+                        <ApplicationLogo
+                            class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                        />
+                    </Link>
+                </div>
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
+                    <div class="flex justify-center h-16">
                         <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
-                                    />
-                                </Link>
-                            </div>
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -57,7 +57,7 @@ const showingNavigationDropdown = ref(false);
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
+                            <div class="ml-3">
                                 <Dropdown align="right" width="48">
                                     <template #trigger>
                                         <span class="inline-flex rounded-md">
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,13 +8,15 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">Dashboard</h2>
+            <h2 id="headline" class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Velkommen til Pandiwebs oversigt over OpenAI!</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Her vil du kunne finde informationer over hvilke modeller der ligger hos OpenAI, 
+                        hvilken træningsdata der er samt mulighed for at lave nye modeller og uploade ny træningsdata.</div>
                 </div>
             </div>
         </div>
