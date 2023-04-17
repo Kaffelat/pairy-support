ad5d8df5d154d73b50227326e38ce0cd2ae82ff5
ghf <simo274e@stud.kea.dk>
2023-04-17 16:18:33 +0200

    Ændrede til transparant billede

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,3 @@
 <template>
-   <img id="logo" class="block h-10 w-auto m-auto" src="/images/Pairy-logo-black.png" alt="Logo" />
+   <img id="logo" class="block h-10 w-auto m-auto" src="/images/Pairy-logo-transperent.png" alt="Logo" />
 </template>
