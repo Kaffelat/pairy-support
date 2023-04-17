504c7b0b78ac8d7ae409c1c739f9e557d753c54b
ghf <simo274e@stud.kea.dk>
2023-04-17 09:17:57 +0200

    Gjort så man kan hente en bestemt fil

--- app/Services/OpenAI/AIModelData.php
+++ app/Services/OpenAI/AIModelData.php
@@ -58,4 +58,20 @@ class AIModelData
 
         return $response->toArray(); 
     }
+
+    public function getAFile($client, $fileId): array
+    {
+        $response = $client->files()->retrieve('file-eFIFEp23oMQuDJ3d6kR58J9i');
+
+        $response->id; // 'file-eFIFEp23oMQuDJ3d6kR58J9i'
+        $response->object; // 'file'
+        $response->bytes; // 140
+        $response->createdAt; // 1613779657
+        $response->filename; // 'mydata.jsonl'
+        $response->purpose; // 'fine-tune'
+        $response->status; // 'succeeded'
+        $response->status_details; // null
+
+        return $response->toArray(); // ['id' => 'file-eFIFEp23oMQuDJ3d6kR58J9i', ...]
+    }
 }
