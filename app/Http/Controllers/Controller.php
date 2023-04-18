d8dc091f100803f94d1dce960c11b078f6e286c8
ghf <simo274e@stud.kea.dk>
2023-04-18 13:06:51 +0200

    a

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AIFile;
+use App\Services\OpenAI\AIFile\AIFilesDownloader;
 use App\Services\OpenAI\AIFile\AIFileService;
 use App\Services\OpenAI\AIFile\DownloadAIFIles;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -18,57 +19,9 @@ class Controller extends BaseController
     #Dette er bare til test af kode. skal ikke bruges i virkeligheden
     public function testGetAllFiles(AIFileService $aiFile)
     {
-        $yourApiKey = getenv('OPENAI_API_KEY');
-        $client = OpenAI::client($yourApiKey);
+        $aiFileDownloader = new AIFilesDownloader;
 
-        $dbFile = AIFile::All();
- 
-        $openAIData = $aiFile->listAllFiles($client);
-        
-        
-        if (empty($dbFile[0]) == false) {
-
-            foreach ($dbFile as $dbFile) {
-                foreach ($openAIData->data as $file) {
-                    if ($dbFile->id != $file->id) {
-                        
-                        $newfile = new AIFile();
-                        
-                        #TODO sæt en klasse på som gør det her
-                        $newfile->fill([ 
-                        'openai_id' => $file->id,
-                        'user_id'   => 1,
-                        'data'      => json_encode(['test for nu']),
-                        'tokens'    => 0,
-                        'validering' => 0,
-                        'traning'   => 1
-                        ]);
-
-                        $newfile->save();
-                    }
-                }
-            }
-        }
-        else {
-            foreach ($openAIData->data as $file) {
-                
-                $newfile = new AIFile();
-                        
-                #TODO sæt en klasse på som gør det her
-                $newfile->fill([ 
-                'openai_id' => $file->id,
-                'user_id'   => 1,
-                'data'      => ['test for nu'],
-                'tokens'    => 0,
-                'validering' => 0,
-                'traning'   => 1
-                ]);
-
-                $newfile->save();
-                }
-            }
-
-        return $openAIData;
-
+        return $aiFileDownloader->getAllFiles($aiFile);
     }
+
 }
