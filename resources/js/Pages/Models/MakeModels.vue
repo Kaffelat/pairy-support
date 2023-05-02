4f6474d962b08bf5b498538c96150847ebb7c828
ghf <simo274e@stud.kea.dk>
2023-05-02 16:50:04 +0200

    Startede på at lave formen til at kunne lave nye modeller

--- resources/js/Pages/Models/MakeModels.vue
+++ resources/js/Pages/Models/MakeModels.vue
@@ -11,9 +11,96 @@ import { Head } from '@inertiajs/vue3';
             <h2 id="headline" class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Make models</h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <form>
+            <label>Training file</label>
+            <input type="text" required v-model = "traningFile">
+
+            <label>Validation file</label>
+            <input type="text" v-model = "validationFile">
+
+            <label>Type</label>
+            <select v-model="type">
+                <option value="davinci">Davinci</option>     
+                <option value="curie">Curie</option>   
+                <option value="babbage">Babbage</option>     
+                <option value="ada">Ada</option>     
+            </select>
+
+            <label>Hvor mange gange skal modellen trænes?</label>
+            <input type="text" v-model ='epochs' >
+
+            <label>Hvad skal dens learningrate være?</label>
+            <input type="text" v-model ='learningRate' >
+
+            <label>Hvad skal den prompt loss være?</label>
+            <input type="text" v-model ='promtLoss' >
+
+            <div class="submit">
+                <button>Create Model</button>
             </div>
-        </div>
+        </form>
     </AuthenticatedLayout>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            traningFile: '',
+            validationFile:'',
+            type:'curie',
+            epochs:'',
+            learningRate:'',
+            promtLoss:''
+        }
+    }
+}
+
+</script>
+
+<style>
+    form {
+        max-width: 420px;
+        margin: auto;
+        background: grey;
+        text-align: left;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    label {
+        color: black;
+        display: inline-block;
+        margin-bottom: 10px;
+        font-size: 1.0em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input {
+        display: block;
+        padding: 10px 6px;
+        margin-bottom: 10px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+
+    button {
+        background: white;
+        display: inline-block;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: black;
+        border-radius: 20px;
+    }
+    .submit {
+        text-align: center;
+    }
+    .submit :hover {
+        background-color: lightgrey;
+        transition: 0.7s;
+    }
+</style>
