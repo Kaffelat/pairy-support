c980b3d3046ee689a66d79bd5fde9ad5034c9734
ghf <simo274e@stud.kea.dk>
2023-05-10 14:07:25 +0200

    test af finetunening af en finetune model

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,10 +10,22 @@ use App\Services\OpenAI\AIModel\UploadAIModel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 use OpenAI;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
+    function test() {
+        $client = OpenAI::client(Auth::user()->openai_api_key);
+        $response = $client->fineTunes()->create([
+            'training_file' => 'file-s4lKxvHWtNLxJBJdXNBkJ226',
+            'model' => 'curie:ft-personal-2023-04-19-12-04-28',
+            'n_epochs' => 4,
+            'prompt_loss_weight' => 0.01,
+        ]);
+        
+        return $response->toArray();
+    }
 }
