66c280a799f2f59d56bb8489411f7b69c53b00c0
ghf <simo274e@stud.kea.dk>
2023-06-07 14:49:05 +0200

    Fiksede så man kan træne modeller igen og fjernet modelForm da det blev for rodet

--- resources/js/Pages/Models/TrainModels.vue
+++ resources/js/Pages/Models/TrainModels.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import ModelForm from '@/Components/ModelForm.vue';
 </script>
 
 <template>
@@ -20,7 +19,32 @@ import ModelForm from '@/Components/ModelForm.vue';
             <label>Model Name</label>
             <input type="text" v-model="type" class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
-            <model-form></model-form>
+            <label>Training File</label>
+            <select required v-model="traningFile" class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option v-for="file in aiFiles" :value="file.openai_id" :key="file.openai_id">
+                    {{ file.openai_id }} {{ selectedFile(file, traningFile) }}
+                </option>
+            </select>
+
+            <label>Validation File</label>
+            <select v-model="validationFile" class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option v-for="file in aiFiles" :value="file.openai_id" :key="file.openai_id">
+                    {{ file.openai_id }} {{ selectedFile(file, validationFile) }}
+                </option>
+            </select>
+
+            <label >Hvor mange epochs skal modellen trænes?</label>
+            <input type="text" v-model='epochs' class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+            <label>Hvad skal modellens learning rate multiplier være?</label>
+            <input type="text" v-model='learningRate' class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+            <label>Hvad skal modellens prompt loss vægt være?</label>
+            <input type="text" v-model='promptLoss' class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+            <div class="submit">
+                <button type="submit" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Create Model</button>
+            </div>
         </form>
     </AuthenticatedLayout>
 </template>
@@ -37,9 +61,10 @@ export default {
             type:this.id,
             traningFile: '',
             validationFile:'',
-            epochs:'4',
-            learningRate:'0.1',
-            promtLoss:'0.01',
+            epochs:4,
+            learningRate:0.1,
+            promptLoss:0.01,
+
             showAlert: false,
             alertMessage: '',
             alertClass: '',
@@ -64,7 +89,7 @@ export default {
                 validationFile: this.validationFile,
                 epochs: this.epochs,
                 learningRate: this.learningRate,
-                promtLoss: this.promtLoss
+                promptLoss: this.promptLoss
             };
             axios.post('/model/upload', data).then(res => {
                 console.log(res.data);
