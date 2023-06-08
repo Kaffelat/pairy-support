3e6178fa5751436755b2181863952ee191c5f078
ghf <simo274e@stud.kea.dk>
2023-06-08 16:35:38 +0200

    typehinting

--- app/Http/Controllers/AIModelResultFileController.php
+++ app/Http/Controllers/AIModelResultFileController.php
@@ -4,24 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Services\OpenAI\AIModelResultFile\AIModelResultFileDownloader;
 use App\Services\OpenAI\AIModelResultFile\AIModelResultFileService;
+use Illuminate\Database\Eloquent\Collection;
+use stdClass;
 
 class AIModelResultFileController 
 {
-    function getAllModelResultFiles(AIModelResultFileService $aiModelResultFileService)
+    function getAllModelResultFiles(AIModelResultFileService $aiModelResultFileService): Collection
     {
         $aiModelResultFileDownloader = new AIModelResultFileDownloader($aiModelResultFileService);
 
         return $aiModelResultFileDownloader->getAllModelResultFiles();
     }
 
-    function downloadModelResultFile(string $resultFileId, AIModelResultFileService $aiModelResultFileService)
+    function downloadModelResultFile(string $resultFileId, AIModelResultFileService $aiModelResultFileService): array
     {
         $aiModelResultFileDownloader = new AIModelResultFileDownloader($aiModelResultFileService);
 
         return $aiModelResultFileDownloader->downloadModelResultFile($resultFileId);
     }
 
-    function getModelResultFile(string $resultFileId, AIModelResultFileService $aiModelResultFileService)
+    function getModelResultFile(string $resultFileId, AIModelResultFileService $aiModelResultFileService): stdClass
     {
         $aiModelResultFileDownloader = new AIModelResultFileDownloader($aiModelResultFileService);
 
